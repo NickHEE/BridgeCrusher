d84caed162c66a81be01f481640149dc9b7a6f73
NickHEE <35848933+NickHEE@users.noreply.github.com>
2018-06-01 09:38:22 -0700

    Newest merged Version

--- TestQT.py
+++ TestQT.py
@@ -3,7 +3,7 @@
 import sys
 from PyQt5 import QtGui, QtCore
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (QWidget, QLCDNumber, QVBoxLayout, QApplication, QGridLayout)
+from PyQt5.QtWidgets import (QWidget, QLCDNumber, QLabel, QApplication, QGridLayout, QPushButton, QLineEdit)
 
 
 class MainWindow(QWidget):
@@ -14,13 +14,25 @@ class MainWindow(QWidget):
 
         def initUI(self):
             self.setGeometry(300, 200, 600, 400)
-            force=1000.1
-            forcelcd = QLCDNumber(6, self)
+
+            #LCD Widget to display the force
+            force=1000.11
+            forcelcd = QLCDNumber(7, self)
+
+            #Push button to zero the scale
+            zero = QPushButton('Zero Scale', self)
+
+            #Textbox to enter in team name
+            teaminput = QLineEdit()
+
+            #Current highest force text
+            maxforce = QLabel()
 
             grid = QGridLayout()
-            grid.setColumnStretch(1, 4)
-            #vbox = QVBoxLayout()
+            grid.setColumnStretch(1, 1)
             grid.addWidget(forcelcd,0,1)
+            grid.addWidget(zero,1,1)
+            grid.addWidget(teaminput, 1,3)
 
             self.updateForce(forcelcd, force)
             self.setLayout(grid)
